3e0e3add620509e8f976f1f94b735fca6fda3140
21020336 <21020336@vnu.edu.vn>
2023-10-03 10:17:41 +0700

    21020336_VuThaiHung

--- search.py
+++ search.py
@@ -87,16 +87,82 @@ def depthFirstSearch(problem: SearchProblem):
     print("Start's successors:", problem.getSuccessors(problem.getStartState()))
     """
     "*** YOUR CODE HERE ***"
+    from game import Directions
+    stack = util.Stack()
+    stack.push(problem.getStartState())
+    parents = {}
+    visited = {}
+    result = []
+    while not(stack.isEmpty()):
+        current_state = stack.pop()
+        if(current_state in visited):
+            continue
+        if(problem.isGoalState(current_state)):
+            while(current_state != problem.getStartState()):
+                next_state, side = parents[current_state]
+                current_state = next_state
+                result.append(side)
+            return result[::-1]
+        visited[current_state] = True
+        for next_state, side, _ in problem.getSuccessors(current_state):
+            if(next_state in visited):
+                continue
+            parents[next_state] = (current_state, side)
+            stack.push(next_state)
     util.raiseNotDefined()
 
 def breadthFirstSearch(problem: SearchProblem):
     """Search the shallowest nodes in the search tree first."""
     "*** YOUR CODE HERE ***"
+    queue = util.Queue()
+    queue.push(problem.getStartState())
+    visited = {}
+    parents = {}
+    visited[problem.getStartState()] = True
+    while not(queue.isEmpty()):
+        current_state = queue.pop()
+        if(problem.isGoalState(current_state)):
+            path = []
+            while(current_state != problem.getStartState()):
+                next_state, side = parents[current_state]
+                path.append(side)
+                current_state = next_state
+            return path[::-1]
+        for next_state, side, _ in problem.getSuccessors(current_state):
+            if(next_state in visited):
+                continue
+            visited[next_state] = True
+            queue.push(next_state)
+            parents[next_state] = (current_state, side)    
     util.raiseNotDefined()
 
 def uniformCostSearch(problem: SearchProblem):
     """Search the node of least total cost first."""
     "*** YOUR CODE HERE ***"
+    fringe = util.PriorityQueue()
+    distances = {}
+    parents = {}
+    distances[problem.getStartState()] = 0
+    fringe.push(problem.getStartState(), 0)
+    while not(fringe.isEmpty()):
+        current_state = fringe.pop()
+        if(problem.isGoalState(current_state)):
+            path = []
+            while(current_state != problem.getStartState()):
+                next_state, side = parents[current_state]
+                current_state = next_state
+                path.append(side)
+            return path[::-1]
+        for next_state, side, cost in problem.getSuccessors(current_state):
+            if not(next_state in distances):
+                distances[next_state] = distances[current_state] + cost
+                fringe.push(next_state, distances[next_state])
+                parents[next_state] = (current_state, side)
+            elif(distances[next_state] > distances[current_state]  + cost):
+                fringe.update(next_state, distances[current_state] + cost)
+                parents[next_state] = (current_state, side)
+                distances[next_state] = distances[current_state] + cost
+
     util.raiseNotDefined()
 
 def nullHeuristic(state, problem=None):
@@ -109,8 +175,30 @@ def nullHeuristic(state, problem=None):
 def aStarSearch(problem: SearchProblem, heuristic=nullHeuristic):
     """Search the node that has the lowest combined cost and heuristic first."""
     "*** YOUR CODE HERE ***"
+    fringe = util.PriorityQueue()
+    distance = {}
+    parents = {}
+    fringe.push(problem.getStartState(), heuristic(problem.getStartState(), problem))
+    distance[problem.getStartState()] = 0
+    while not(fringe.isEmpty()):
+        current_state = fringe.pop()
+        if(problem.isGoalState(current_state)):
+            path = []
+            while(current_state != problem.getStartState()):
+                next_state, side = parents[current_state]
+                path.append(side)
+                current_state = next_state
+            return path[::-1]
+        for next_state, side, cost in problem.getSuccessors(current_state):
+            if(next_state not in distance):
+                distance[next_state] = distance[current_state] + cost
+                fringe.push(next_state, distance[next_state] + heuristic(next_state, problem))
+                parents[next_state] = (current_state, side)
+            elif(distance[next_state]  > distance[current_state]  + cost):
+                distance[next_state] = distance[current_state] + cost
+                fringe.update(next_state, distance[next_state] + heuristic(next_state, problem))
+                parents[next_state] = (current_state, side)
     util.raiseNotDefined()
-
 
 # Abbreviations
 bfs = breadthFirstSearch
